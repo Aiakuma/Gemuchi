509b999c0c4b2840d39c3a960f6aa46273703ee8
Aiakuma <68282500+Aiakuma@users.noreply.github.com>
2022-01-19 15:28:18 +0100

    Mise à jour

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,16 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\GameRepository;
+use App\Repository\CategoryRepository;
+use App\Repository\ConsoleRepository;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
      */
-    public function index(): Response
+    public function index(GameRepository $GameRepository, CategoryRepository $categoryRepository,ConsoleRepository $consoleRepository): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        $games = $GameRepository->findBy([], ['id' => 'DESC']);
+        $category = $categoryRepository->findAll();
+        $consoles = $consoleRepository->findAll();
+
+        return $this->render('home/index.html.twig',
+            [
+                'games' => $games,
+                'categories' => $category,
+                'consoles' => $consoles,
+            ]);
     }
 }
